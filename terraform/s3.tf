e095295dea615dfb26c42ad0c10e7084db79629b
Pat Heard <patrick.heard@cds-snc.ca>
2025-11-12 17:11:19 -0500

    feat: setup Terraform for existing infrastructure
    Setup the Terraform and GitHub workflows required to manage
    the existing backup infrastructure.

---
+++ terraform/s3.tf
@@ -0,0 +1,13 @@
+module "backup_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v10.8.1"
+  bucket_name       = local.bucket_name
+  billing_tag_value = local.billing_tag_value
+
+  lifecycle_rule = [
+    local.lifecycle_expire_all
+  ]
+
+  versioning = {
+    enabled = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "account_id" {
+  description = "The AWS account ID to deploy resources in"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
