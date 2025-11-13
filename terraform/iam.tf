8adbbabfbadbd76a8c529ac9385689442904d52e
Pat Heard <patrick.heard@cds-snc.ca>
2025-11-13 14:19:44 +0000

    feat: add OIDC role for GitHub backup write
    Add a new IAM role that any CDS repo can use to write to the S3 backup bucket.
    
    Also fixes the Terraform apply action to call `terraform init` before appying changes.

---
+++ terraform/iam.tf
@@ -0,0 +1,47 @@
+locals {
+  github_backup_write = "github-backup-write"
+}
+
+module "github_workflow_roles" {
+  source            = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v10.8.1"
+  billing_tag_value = local.billing_tag_value
+  roles = [
+    {
+      name      = local.github_backup_write
+      repo_name = "*" # Allow any CDS repo to use this role
+      claim     = "ref:refs/heads/main"
+    }
+  ]
+}
+
+#
+# Allow GitHub workflows in CDS repos to write data to the backup bucket
+#
+resource "aws_iam_role_policy_attachment" "github_backup_write" {
+  role       = local.github_backup_write
+  policy_arn = aws_iam_policy.github_backup_write.arn
+  depends_on = [
+    module.github_workflow_roles
+  ]
+}
+
+resource "aws_iam_policy" "github_backup_write" {
+  name   = local.github_backup_write
+  policy = data.aws_iam_policy_document.github_backup_write.json
+}
+
+data "aws_iam_policy_document" "github_backup_write" {
+  statement {
+    sid = "S3ReadWrite"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      module.backup_bucket.bucket_arn,
+      "${module.backup_bucket.bucket_arn}/*"
+    ]
+  }
+}
